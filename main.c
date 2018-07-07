0ae333af60747191a00ff2ed6860a6bbf6c33b07
rlarjsdml12 <40611306+rlarjsdml12@users.noreply.github.com>
2018-07-07 13:42:10 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,17 @@
-#include <stdio.h>
-cout << "enter the number: "
+#include<hidef.h>
+#include "derivative.h"
+
+int main(void)
+{
+  SOPT_COPE = 0;
+  SCI28DH = 0x00;
+  SCI28DL = 0x82;
+  SCI2C2 = 0x0c;
+  for(;;)
+  {
+    while((SCI251 & 0x20)! = 0x20);
+    while((SCI251 & 0x80)! = 0x80);
+    SCI2D = SCI2D;
+    LEDG = SCI2D;
+  }
+}
